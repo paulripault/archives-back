2d9eaab69a462bdc09bbdead9b53180182c373b6
Paul <paulripault0@gmail.com>
2026-06-14 11:18:08 +0200

    building backend app

---
+++ src/main/java/com/archives/backend/archives_backend/ArchivesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.archives.backend.archives_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archives/backend/archives_backend/ArchivesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archives.backend.archives_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
